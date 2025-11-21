a6da72fd992ebd82b221c7bc3996f85152d6c5bb
bbakkifoundation-cmyk <bbakkifoundation@gmail.com>
2025-11-21 21:49:49 +0530

    Add files via upload

---
+++ adv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import joblib
+import stremlit as st
+model =joblib.load(open("linear_regression_model.joblib",'rb'))
+st.title("sales prediction app")
+
+TV=st.number_input("TV Adv Budget",min_value=0.0)
+Radio=st.number_input("Radio Adv Budget",min_value=0.0)
+NEWSPAPER=st.number_input("NewsPaper Budget",min_value=0.0)
+
+if st.button('predict sales'):
+	input_data=np.arrary([[TV,RADIO,NEWSPAPER]])
+	prediction=model.predict(input_data)[0]
+st.sucess(f'predict sales:{prediction:.2f}')
