c4f69f20d21903cad0b551ee7603987732c62c8e
bbakkifoundation-cmyk <bbakkifoundation@gmail.com>
2025-11-21 21:55:18 +0530

    Fix imports, variable names, and prediction output
    Fixed import statement for Streamlit and corrected variable name for RADIO. Also, fixed typo in 'array' and added success message for prediction.

--- adv.py
+++ adv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import joblib
-import stremlit as st
+import streamlit as st
 model =joblib.load(open("linear_regression_model.joblib",'rb'))
 st.title("sales prediction app")
 
@@ -12,4 +12,5 @@ NEWSPAPER=st.number_input("NewsPaper Budget",min_value=0.0)
 if st.button('predict sales'):
 	input_data=np.arrary([[TV,RADIO,NEWSPAPER]])
 	prediction=model.predict(input_data)[0]
+
 st.sucess(f'predict sales:{prediction:.2f}')
